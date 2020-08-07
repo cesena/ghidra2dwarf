502870fa927e23d789ca9b6678850f20e00ed821
meowmeowxw <giovanni.disanti@protonmail.com>
2020-08-07 16:50:46 +0200

    Add dwarfexport init

---
+++ script/dwarfexport.py
@@ -0,0 +1,29 @@
+# Ghidra2Dwarf
+# @author sen, meowmeowxw
+# @category _NEW_
+# @keybinding
+# @menupath
+# @toolbar
+
+try:
+    from ghidra_builtins import *
+except:
+    pass
+
+
+from ghidra.app.decompiler import DecompInterface
+from ghidra.util.task import ConsoleTaskMonitor
+from ghidra.app.util.bin.format.elf import ElfSymbolTable
+
+from libdwarf import LibdwarfLibrary
+from com.sun.jna.ptr import PointerByReference
+
+l = LibdwarfLibrary.INSTANCE
+
+g = globals()
+for i in LibdwarfLibrary.__dict__.keys():
+    g[i] = getattr(l, i)
+
+print DW_DLE_DWARF_INIT_DBG_NULL
+print DW_DLE_HEX_STRING_ERROR
+

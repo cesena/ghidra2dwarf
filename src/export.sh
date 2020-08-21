2049c8763f9060e64e4359488f16991b95cb09b9
meowmeowxw <giovanni.disanti@protonmail.com>
2020-08-21 20:43:50 +0200

    Update export script

--- src/export.sh
+++ src/export.sh
@@ -7,8 +7,12 @@ BINARY_NEW=${BINARY}_dbg
 echo ${BINARY_PATH} ${BINARY}
 cd $BINARY_PATH
 cp $BINARY $BINARY_NEW
+
 # Remove unneded sections...
+echo '[*] Removing unneeded debug sections'
 objcopy -g $BINARY_NEW
+
+echo '[*] Adding the debug sections'
 objcopy --add-section .debug_info=.debug_info $BINARY_NEW
 objcopy --add-section .debug_line=.debug_line $BINARY_NEW
 objcopy --add-section .debug_abbrev=.debug_abbrev $BINARY_NEW

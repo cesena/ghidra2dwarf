da7055b617124d25391a260cb542b5a413065252
meowmeowxw <giovanni.disanti@protonmail.com>
2021-08-27 10:54:46 +0200

    Remove 'if' in fetch_libs_and_build

--- lib/fetch_libs_and_build.sh
+++ lib/fetch_libs_and_build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if [ ! -f src/main/resources/linux-x86-64/libdwarf.so ]; then
-	curl -o src/main/resources/linux-x86-64/libdwarf.so -L --create-dirs https://github.com/cesena/libdwarf-ghidra2dwarf/releases/download/latest/libdwarf.so
-fi
-if [ ! -f src/main/resources/win32-x86-64/libdwarf.dll ]; then
-	curl -o src/main/resources/win32-x86-64/libdwarf.dll -L --create-dirs https://github.com/cesena/libdwarf-ghidra2dwarf/releases/download/latest/libdwarf.dll
-fi
-if [ ! -f src/main/resources/darwin/libdwarf.dylib ]; then
-	curl -o src/main/resources/darwin/libdwarf.dylib -L --create-dirs https://github.com/cesena/libdwarf-ghidra2dwarf/releases/download/latest/libdwarf.dylib
-fi
+[ ! -f src/main/resources/linux-x86-64/libdwarf.so ] && \
+    curl -o src/main/resources/linux-x86-64/libdwarf.so -L --create-dirs https://github.com/cesena/libdwarf-ghidra2dwarf/releases/download/latest/libdwarf.so
+
+[ ! -f src/main/resources/win32-x86-64/libdwarf.dll ] && \
+    curl -o src/main/resources/win32-x86-64/libdwarf.dll -L --create-dirs https://github.com/cesena/libdwarf-ghidra2dwarf/releases/download/latest/libdwarf.dll
+
+[ ! -f src/main/resources/darwin/libdwarf.dylib ] && \
+    curl -o src/main/resources/darwin/libdwarf.dylib -L --create-dirs https://github.com/cesena/libdwarf-ghidra2dwarf/releases/download/latest/libdwarf.dylib
 
 mvn package
